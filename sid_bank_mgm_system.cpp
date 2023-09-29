027548cfb31d2bf4662d9a9d857a74509ddaec4b
Siddharth Kar <146120624+JeansPanT@users.noreply.github.com>
2023-09-29 16:12:38 +0530

    BANK MANAGEMENT SYSTEM 1.0
    Bank Management System by Sid
    
    Features - 
    Normal & Business Mode
    
    Customer information and Bank activities

---
+++ sid_bank_mgm_system.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+using namespace std;
+class bank
+{
+public:
+    char bname[100],ifsc[100];
+    void get()
+    {
+
+    }
+};
+class customer : public bank
+{
+public:
+    char name[100],add[100];
+    void get()
+    {
+        cout<<"\n Enter your name      :"<<endl;
+        cin>>name;
+        cout<<"\n Enter your address   :"<<endl;
+        cin>>add;
+        cout<<"\n Enter your bank name :"<<endl;
+        cin>>bname;
+        cout<<"\n Enter your IFSC Code :"<<endl;
+        cin>>ifsc;
+    }
+};
+class normal : virtual public customer
+{
+public:
+    char accno[100],acctype[100];
+    void get1()
+    {
+        cout<<"\n Enter your account no. :"<<endl;
+        cin>>accno;
+        cout<<"\n Enter your account type [ Saving / Current ]  : "<<endl;
+        cin>>acctype;
+    }
+    void show()
+    {
+        cout<<"\n Customer Name : "<<name;
+        cout<<"\n Bank Name     : "<<bname;
+        cout<<"\n Address       : "<<add;
+        cout<<"\n IFSC Code     : "<<ifsc;
+        cout<<"\n Account No.   : "<<accno;
+        cout<<"\n Account Type. : "<<acctype;
+    }
+};
+class business : virtual public customer
+{
+public:
+    char ano[100],atype[100];
+    void get2()
+    {
+        cout<<"\n Enter your account no. :"<<endl;
+        cin>>ano;
+        cout<<"\n Enter your account type [ Saving / Current / Overseas ]  : "<<endl;
+        cin>>atype;
+    }
+    void show()
+    {
+        cout<<"\n Customer Name : "<<name;
+        cout<<"\n Bank Name     : "<<bname;
+        cout<<"\n Address       : "<<add;
+        cout<<"\n IFSC Code     : "<<ifsc;
+        cout<<"\n Account No.   : "<<ano;
+        cout<<"\n Account Type. : "<<atype;
+    }
+};
+class bank_act : public bank
+{
+public:
+    int p,r,t,si;
+    char loan[100];
+    void get()
+    {
+        cout<<"\n Enter the type of Loan [ Personal / House / AL ] :";
+        cin>>loan;
+        cout<<"\n Enter Principle Amount :";
+        cin>>p;
+        cout<<"\n Enter Rate of Interest :";
+        cin>>r;
+        cout<<"\n Enter loan duration    :";
+        cin>>t;
+    }
+    void show()
+    {
+        si=(p*r*t)/100;
+        cout<<"\n Loan type             :"<<loan;
+        cout<<"\n Loan Amount           :"<<p;
+        cout<<"\n Loan Rate of Interest :"<<r<<"%";
+        cout<<"\n Loan duration         :"<<t<<"Years";
+        cout<<"\n Loan Interest Amount  :"<<si;
+    }
+};
+int main()
+{
+    int no,num;
+    normal n;
+    business b;
+    bank_act ba;
+    cout<<"\n Bank Management System by Sid 1.0"<<endl;
+    cout<<"\n 1. Update Customer Details -"<<"\n 2. Check Bank Activities -"<<"\n 3. Exit -"<<endl;
+    cout<<"\n Enter your choice :";
+    cin>>no;
+
+    switch(no)
+    {
+    case 1:
+        {
+            cout<<"\n 8. Normal Customer -"<<"\n 9. Businessman -";
+            cout<<"\n Enter your choice :";
+            cin>>num;
+
+            switch(num)
+            {
+            case 8:
+                {
+                    n.customer::get();
+                    n.normal::get1();
+                    n.normal::show();
+                    break;
+                }
+            
+            case 9:
+                {
+                    b.customer::get();
+                    b.business::get2();
+                    b.business::show();
+                    break;
+                }
+                break;
+            }
+            break;
+        }
+    case 2:
+        {
+            ba.bank_act::get();
+            ba.bank_act::show();
+            break;
+        }
+    case 3:
+        {
+            cout<<"\n You've successfully exited . . .";
+            break;
+        }
+    default:
+        {
+            cout<<"Invalid choice";
+            break;
+        }
+    }
+}
